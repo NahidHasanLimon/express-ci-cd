880229724356f13ceb273c2405507eecc6a1d6ec
Md. Nahid Hasan Limon <41167616+NahidHasanLimon@users.noreply.github.com>
2024-06-14 16:48:55 +0600

    sample app js added

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to log requests
+app.use((req, res, next) => {
+  console.log(`${req.method} request received at ${req.url}`);
+  next();
+});
+
+// Route for the homepage
+app.get('/', (req, res) => {
+  res.send('Hello from the Express app!');
+});
+
+// Route with a path parameter
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`User with ID: ${userId}`);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
